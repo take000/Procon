0444baba781eada4560516987e2bc62e25941cd5
rit3919 <41986326+rit3919@users.noreply.github.com>
2019-09-15 23:24:07 +0900

    Update gui.cpp
    エージェントの状態と更新ボタン、入力ボタン、ソルバーのメッセージボックス（ただの図形）を追加

--- gui.cpp
+++ gui.cpp
@@ -1,6 +1,4 @@
 # include <Siv3D.hpp> // OpenSiv3D v0.3.2
-#include<iostream>
-#include<ctime>
 #include<vector>
 
 
@@ -83,10 +81,21 @@ int teamID = 5, enemyID = 6;
 //領域及び範囲ポイント//
 int mytile = 11, enemytile = 12;
 int myarea = 13, enemyarea = 14;
-
+//agent//
+int agent = 8;
+int agentID[8] = { 1,2,3,4,5,6,7,8 };
+String agentSt[8] = { U"move",U"remove",U"stay",U"move",U"remove",U"stay" ,U"move",U"remove" };
+	//agent_coord//
+int agent_coord[8][2] = {
+	{2,8},{11,16},{1,8},{17,14},
+	{13,19},{5,17},{8,13},{6,6},
+};
+//turn//
+int turn = 1;
+//マウスオーバー//
 void Main()
 {
-	/*int vy=14, vt=17;*/
+	//font定義//
 	const Font font(25);
 	const Font p_font(50);
 	const Font sougouf(60);
@@ -96,7 +105,6 @@ void Main()
 	Window::Resize(1280, 720);
 	//名前の変更
 	Window::SetTitle(U"Procon30_kagawa");
-
 	while (System::Update())
 	{
 		//更新点滅//
@@ -106,7 +114,7 @@ void Main()
 		Rect(450, 10, 380, 70).draw(Palette::Orange);
 		//ポイント表示//
 		//味方//
-		p_font(mytile,U"+",myarea).draw(Arg::bottomLeft = Vec2(23, 90), Palette::White);
+		p_font(mytile, U"+", myarea).draw(Arg::bottomLeft = Vec2(23, 90), Palette::White);
 		font(U"総合                　　　　　総合").draw(Arg::bottomLeft = Vec2(250, 78), Palette::White);
 		font(U"タイル     領域　　　　　　　　　　　　　　　　　　   タイル     領域").draw(Arg::bottomLeft = Vec2(20, 40), Palette::White);
 		sougouf(mytile + myarea).draw(Arg::bottomLeft = Vec2(300, 90), Palette::White);
@@ -122,13 +130,13 @@ void Main()
 					Rect(10 + (j * 41), 90 + (i * 31), 40, 30).draw(Palette::White);
 				//タイルがチームIDの場合:青
 				else if (tile[i][j] == teamID)
-					if (kousin[i][j] == teamID) 
+					if (kousin[i][j] == teamID)
 						Rect(10 + (j * 41), 90 + (i * 31), 40, 30).draw(Palette::Dodgerblue).draw(AlphaF(animation));
 					else
 						Rect(10 + (j * 41), 90 + (i * 31), 40, 30).draw(Palette::Dodgerblue);
 				//タイルが敵チームIDの場合:オレンジ
 				else if (tile[i][j] == enemyID) {
-					if(kousin[i][j] == enemyID)
+					if (kousin[i][j] == enemyID)
 						Rect(10 + (j * 41), 90 + (i * 31), 40, 30).draw(Palette::Orange).draw(AlphaF(animation));
 					else
 						Rect(10 + (j * 41), 90 + (i * 31), 40, 30).draw(Palette::Orange);
@@ -139,8 +147,30 @@ void Main()
 		//フィールドを生成
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				font(point[i][j]).draw(Arg::bottomLeft = Vec2(10+ (j * 41), 120 + (i * 31)), Palette::Black);
+				font(point[i][j]).drawAt(Vec2(30 + (j * 41), 105 + (i * 31)), Palette::Black);
 			}
 		}
+		//turn//
+		p_font(U"turn:", turn).draw(Vec2(850, 20), Palette::White);
+		//agent status//
+		for (int i = 0; i < agent; i++) {
+			if (i != 0 && i % 2 != 0) {
+				font(U"agent:", agentID[i], U" ", agentSt[i]).draw(Vec2(1070, 100 + (i - 1) * 40), Palette::White);
+			}
+			else
+				font(U"agent:", agentID[i], U" ", agentSt[i]).draw(Vec2(850, 100 + i * 40), Palette::White);
+		}
+		//更新ボタン//
+		if (SimpleGUI::Button(U"更新", Vec2(850, 400), 180)) {
+
+		}
+		//入力ボタン//
+		if (SimpleGUI::Button(U"入力", Vec2(1070, 400), 180)) {
+
+		}
+		//ソルバーメッセージ//
+		font(U"solver status").draw(Vec2(850,450), Palette::White);
+		Rect(850, 490, 420, 200).draw(Palette::Black);
 	}
+
 }
